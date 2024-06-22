c9bb7ea22b205d549869e3b18c41e97aa99942c3
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-06-23 00:35:42 +0300

    revert impeller

--- android/src/main/java/com/mediadevkit/fvp/FvpPlugin.java
+++ android/src/main/java/com/mediadevkit/fvp/FvpPlugin.java
@@ -33,7 +33,7 @@ public class FvpPlugin implements FlutterPlugin, MethodCallHandler {
   private MethodChannel channel;
   // https://api.flutter.dev/javadoc/io/flutter/view/TextureRegistry.html
   private TextureRegistry texRegistry;
-  private Map<Long, TextureEntry> textures; // SurfaceProducer or SurfaceTextureEntry
+  private Map<Long, SurfaceTextureEntry> textures; // SurfaceProducer or SurfaceTextureEntry
   private Map<Long, Surface> surfaces;
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -52,15 +52,15 @@ public class FvpPlugin implements FlutterPlugin, MethodCallHandler {
       final int width = (int)call.argument("width");
       final int height = (int)call.argument("height");
       final boolean tunnel = (boolean)call.argument("tunnel");
-/*
+
       SurfaceTextureEntry te = texRegistry.createSurfaceTexture();
       SurfaceTexture tex = te.surfaceTexture();
       tex.setDefaultBufferSize(width, height); // TODO: size from player. rotate, fullscreen change?
       Surface surface = new Surface(tex); // TODO: when to release
-*/
-      SurfaceProducer te = texRegistry.createSurfaceProducer();
-      te.setSize(width, height);
-      Surface surface = te.getSurface();
+
+      // SurfaceProducer te = texRegistry.createSurfaceProducer();
+      // te.setSize(width, height);
+      // Surface surface = te.getSurface();
 
       long texId = te.id();
       nativeSetSurface(handle, texId, surface, width, height, tunnel);
@@ -71,7 +71,7 @@ public class FvpPlugin implements FlutterPlugin, MethodCallHandler {
       final int texId = call.argument("texture"); // 32bit int, 0, 1, 2 .... but SurfaceTexture.id() is long
       final long texId64 = texId; // MUST cast texId to long, otherwise remove() error
       nativeSetSurface(0, texId, null, -1, -1, false);
-      TextureEntry te = textures.get(texId64);
+      SurfaceTextureEntry te = textures.get(texId64);
       if (te == null) {
         Log.w("FvpPlugin", "onMethodCall: ReleaseRT texId not found: " + texId);
       } else {
