ce43748e160fc45026505537220b2a9f0aa1c733
wangbin <wbsecg1@gmail.com>
2026-05-21 17:55:00 +0800

    support SPM

---
+++ darwin/fvp/Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "fvp",
+    platforms: [
+        .iOS("12.0"),
+        .macOS("10.13"),
+    ],
+    products: [
+        .library(name: "fvp", targets: ["fvp"]),
+    ],
+    dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework"),
+    ],
+    targets: [
+        .target(
+            name: "fvp",
+            dependencies: [
+                .target(name: "mdk"),
+                .product(name: "FlutterFramework", package: "FlutterFramework"),
+            ],
+            path: ".",
+            sources: [
+                "Sources/fvp/FvpPlugin.mm",
+                "Sources/fvp/callbacks.cpp",
+            ],
+            resources: [
+                .process("Resources/PrivacyInfo.xcprivacy"),
+            ],
+            publicHeadersPath: "Sources/fvp",
+            cSettings: [
+                .headerSearchPath("Sources/fvp"),
+            ],
+            cxxSettings: [
+                .unsafeFlags(["-Wno-documentation"]),
+            ],
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("Metal"),
+            ]
+        ),
+        .binaryTarget(
+            name: "mdk",
+            url: "https://github.com/wang-bin/mdk-sdk/releases/download/v0.36.0/mdk-sdk-apple.zip",
+            checksum: "9c73e96a113096c9e8d9d255477e5327301bff6dead2266f7ab5436d37d6ccb6"
+        ),
+    ],
+    cxxLanguageStandard: .cxx20
+)
