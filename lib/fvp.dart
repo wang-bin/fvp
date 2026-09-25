7b685d670e0cb51014c87322e710553fddb0bd7d
Julien <ju.vermet@gmail.com>
2026-09-25 11:28:20 +0200

    fix: cancel pending player creation before disposal

--- lib/fvp.dart
+++ lib/fvp.dart
@@ -7,6 +7,7 @@ import 'src/video_player_mdk.dart'
     if (dart.library.html) 'src/video_player_dummy.dart';
 
 export 'src/controller.dart';
+export 'src/create_cancellation.dart' show FvpCreateCancellation;
 
 /// Registers this plugin as the default instance of VideoPlayerPlatform. Then your [VideoPlayer] will support all platforms.
 /// If registerWith is not called, the previous(usually official) implementation will be used when available.
